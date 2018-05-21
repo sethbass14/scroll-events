5617d3efd192be24979c5a0d40d9a2337c105b08
sethbass14 <github email address>
2018-05-21 18:24:50 -0400

    adding grid

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const myBtn = document.getElementById('myBtn');
   const span = document.getElementsByClassName('close')[0]
   const form = document.getElementById('sign-up-form')
-  
+
   myBtn.addEventListener('click', () => openModal(modal) )
   span.addEventListener('click', () => closeModal(modal))
   form.addEventListener('submit', (event) => handleSubmit(form, modal))
@@ -16,20 +16,19 @@ const openModal = modal => {
 const closeModal = modal => {
   const modalContent = modal.firstElementChild
   modalContent.style.animationName = 'disappear';
-  modal.style.animationName = 'fade';
+  modal.style.animationName = 'fade-out';
   setTimeout(() => {
-    modalContent.style.animationName = 'animatetop';
+    modalContent.style.animationName = 'topdrop';
     modal.style.display = 'none';
-    modal.style.animationName = ''  
+    modal.style.animationName = 'fade-in'
   }, 1000)
 }
 
 const handleSubmit = (form, modal) => {
   event.preventDefault()
-  const name = form.elements.name.value 
+  const name = form.elements.name.value
   const email = form.elements.email.value
-  const submit = form.elements.submit
-  alert(`Hey ${name}! Your password is ${email}!`)
+  alert(`Hey ${name}! Your email is ${email}!`)
   closeModal(modal)
   form.reset()
 }
