5cdcf0bb6bad87813dfd7b8d9d60a95b2ae44020
sethbass14 <github email address>
2018-05-21 21:45:51 -0400

    the modal can be closed

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const navBar = document.getElementsByTagName('nav')[0]
   const scrollDef = document.getElementById('scroll-definitions')
   const mrTModal = document.getElementsByClassName('modal')[0]
+  const closeBtn = document.getElementById('close')
   
   document.addEventListener('scroll', () => scrollHandler(navBar, scrollDef, mrTModal))
+  closeBtn.addEventListener('click', () => closeModal(mrTModal))
 })
 
 const scrollHandler = (navEl, limitEl, mrTModal) => {
@@ -34,16 +36,16 @@ const letsScroll = mrTModal => {
 const openModal = modal => {
   modal.style.display = 'block';
 }
-// 
-// const closeModal = modal => {
-//   const modalContent = modal.firstElementChild
-//   modalContent.style.animationName = 'disappear';
-//   modal.style.animationName = 'fade-out';
-//   setTimeout(() => {
-//     modalContent.style.animationName = 'topdrop';
-//     modal.style.display = 'none';
-//     modal.style.animationName = 'fade-in'
-//   }, 1000)
-// }
+
+const closeModal = modal => {
+  modal.style.display = 'none';
+  // modalContent.style.animationName = 'disappear';
+  // modal.style.animationName = 'fade-out';
+  // setTimeout(() => {
+  //   modalContent.style.animationName = 'topdrop';
+  //   modal.style.display = 'none';
+  //   modal.style.animationName = 'fade-in'
+  // }, 1000)
+}
 
 
